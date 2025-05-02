cbbb9e4f11f3b3d108273608c4a4e25ed2954865
ernesto2011 <skillmx107@gmail.com>
2025-05-01 22:26:39 -0600

    :rocket: iniciando proyecto de flutter del curso de platzi

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/sreens/home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: RecipeBook(),
+    );
+  }
+}
+
+class RecipeBook extends StatelessWidget {
+  const RecipeBook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal[300],
+          title: Text(
+            'Recipe Book',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white,
+            tabs: [
+            Tab(icon:Icon(Icons.home), text: 'Home'),
+          ]),
+        ),
+        body:TabBarView(children:[HomeScreen()] )
+      ),
+    );
+  }
+}
